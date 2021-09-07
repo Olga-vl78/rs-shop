9b5765faa748226a2373cab9facca93b2b5061fb
Olga-vl78 <olga@butovich.com>
2021-09-07 10:36:37 +0300

    feat: add backend-service

---
+++ rs-shop/src/app/core/services/backend.service.ts
@@ -0,0 +1,63 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ICategory } from 'src/app/shared/models/category.model';
+
+
+const BASEURL = 'http://localhost:3004';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackendService {
+
+  categoriesUrl: string = `${BASEURL}/categories`;
+
+  constructor(private readonly http: HttpClient) {
+  }
+
+  fetchCategories() {
+    return this.http.get<ICategory[]>(`${BASEURL}/categories`)
+      .pipe(
+        catchError(this.handleError))
+      .toPromise();
+  }
+
+  fetchSearchGoods() {
+    return this.http.get<ICategory>(`${BASEURL}/goods/search?text=searchQuery`)
+      .pipe(
+        catchError(this.handleError))
+    //.toPromise();
+  }
+
+  fetchCategory(catId: string) {
+    return this.http.get<ICategory>(`${BASEURL}/goods/category/${catId}`)
+      .pipe(
+        catchError(this.handleError))
+    //.toPromise();
+  }
+
+  fetchSubcategory(catId: string, subcatId: string) {
+    return this.http.get<ICategory>(`${BASEURL}/goods/category/${catId}/${subcatId}`)
+      .pipe(
+        catchError(this.handleError))
+    //.toPromise();
+  }
+
+  fetchItem(itemId: string) {
+    return this.http.get<ICategory>(`${BASEURL}/goods/item/${itemId}`)
+      .pipe(
+        catchError(this.handleError))
+    //.toPromise();
+  }
+
+  handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('An error occurred:', error.error);
+    } else {
+      console.error(`Backend returned code ${error.status}, body was:`, error.error);
+    }
+    return throwError('Something bad happens; please try again later');
+  }
+}
