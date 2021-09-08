8c9a07790fbd02b69ee998aa3de5ec0e560612ce
Olga-vl78 <olga@butovich.com>
2021-09-08 11:07:11 +0300

    feat: add routing, add catalog-page

--- rs-shop/src/app/app-routing.module.ts
+++ rs-shop/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//import { CatalogPageComponent } from './goods/pages/catalog-page/catalog-page.component';
+import { MainPageComponent } from './main/pages/main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'main',
+    component: MainPageComponent,
+  },
+  {
+    path: 'catalog',
+    //component: CatalogPageComponent,
+    loadChildren: () => import('./goods/goods.module').then((m) => m.GoodsModule),
+  },
+  /*{
+    path: 'categories',
+    component: CategoryPageComponent,
+  },*/
+  {
+    path: '**',
+    redirectTo: 'main',
+  },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- rs-shop/src/app/goods/goods.module.ts
+++ rs-shop/src/app/goods/goods.module.ts
@@ -1,12 +1,22 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CategoriesListComponent } from './components/categories-list/categories-list.component';
+import { CatalogPageComponent } from './pages/catalog-page/catalog-page.component';
 
-
+const routes: Routes = [
+  { path: '', component: CatalogPageComponent, pathMatch: 'full' },
+  //{ path: '', component: CategoryPageComponent, pathMatch: 'full' },
+  //{ path: ':id', component: DetailedInfoPageComponent },
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CatalogPageComponent,
+    CategoriesListComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule, RouterModule.forChild(routes)
   ]
 })
 export class GoodsModule { }
