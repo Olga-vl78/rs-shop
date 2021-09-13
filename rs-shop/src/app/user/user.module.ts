4b03ef2ef39623c362b05ef9e3c8df86fefe7a3f
Olga-vl78 <olga@butovich.com>
2021-09-13 21:41:36 +0300

    feat: add order-form component

--- rs-shop/src/app/user/user.module.ts
+++ rs-shop/src/app/user/user.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { OrdersContainerComponent } from './components/orders-container/orders-container.component';
 import { OrdersTableComponent } from './components/orders-table/orders-table.component';
 import { OrderPageComponent } from './pages/order-page/order-page.component';
+import { OrderFormComponent } from './components/order-form/order-form.component';
 
 const routes: Routes = [
   { path: '', component: OrderPageComponent, pathMatch: 'full' },
@@ -13,7 +14,8 @@ const routes: Routes = [
   declarations: [
     OrdersTableComponent,
     OrdersContainerComponent,
-    OrderPageComponent
+    OrderPageComponent,
+    OrderFormComponent
   ],
   imports: [
     CommonModule, RouterModule.forChild(routes)
